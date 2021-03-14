9546934c7e5378e1c34d37c85199f77932b2b2dd
duongngt <duongngt16@gmail.com>
2021-03-15 06:31:44 +0700

    upload fix last

--- js/index.js
+++ js/index.js
@@ -8,3 +8,22 @@ window.onload = function(){
 		menu.style.display=(menu.style.display=="none")? "block" : "none";
 	}
 }
+function scrollToTop (duration) {
+    // cancel if already on top
+    if (document.scrollingElement.scrollTop === 0) return;
+
+    const totalScrollDistance = document.scrollingElement.scrollTop;
+    let scrollY = totalScrollDistance, oldTimestamp = null;
+
+    function step (newTimestamp) {
+        if (oldTimestamp !== null) {
+            // if duration is 0 scrollY will be -Infinity
+            scrollY -= totalScrollDistance * (newTimestamp - oldTimestamp) / duration;
+            if (scrollY <= 0) return document.scrollingElement.scrollTop = 0;
+            document.scrollingElement.scrollTop = scrollY;
+        }
+        oldTimestamp = newTimestamp;
+        window.requestAnimationFrame(step);
+    }
+    window.requestAnimationFrame(step);
+}
